9ec9ef46a69f2864ebee4dd86beb1158ac364e44
inyoka <simon@inyoka.uk>
2020-05-16 15:40:30 +0100

    Import flask_restful and test.

--- simpleapi.py
+++ simpleapi.py
@@ -1,7 +1,19 @@
+#!/usr/bin/env python3.8
 
 from flask import Flask
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
+
+
+class CompanyTimeline(Resource):
+
+    def get(self):
+        return {'text':'it works!'}
+
+
+api.add_resource(CompanyTimeline, '/')
 
 
 if __name__ == '__main__':
